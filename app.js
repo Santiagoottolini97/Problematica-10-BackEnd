bdda210c0c9d1dc303e2d9cb2508f5c879d0c2b6
santiago <santiago.ottolini97@hotmail.com>
2021-04-14 10:42:17 -0300

    P10: Use router an change port 3000 to 4000

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("http");
 const app = require("./index");
 
 //process.env.PORT = validation, if the port not exist assign a port, in this case 3000
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const server = http.createServer(app);
 
 server.listen(port);
--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const express = require("express");
 
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000");
+app.listen(4000, () => {
+  console.log("Listening on port 4000");
 });
