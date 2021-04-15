59ef3f150d320c46a84377b9a696fcb124fc11d7
santiago <santiago.ottolini97@hotmail.com>
2021-04-15 11:34:36 -0300

    P10: Install cors an body parser

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
 const express = require("express");
-const userRoutes = require("./src/routes/security");
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const port = process.env.PORT || 4000;
 const app = express();
 
-app.use("/", userRoutes);
+//When we have a forms, with this line can accepts this forms html, extended false said that only
+//recive json format, not image or video for instance
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.listen(4000, () => {
-  console.log("Listening on port 4000");
+//CORS
+app.use(cors());
+
+const users = {
+  fullName: "",
+  email: "",
+  password: "",
+  passwordR: "",
+};
+
+app.get("/get", (req, res) => {
+  res.status(200).send(users);
+});
+
+app.post("/postUser", (req, res) => {
+  res.send("Succes");
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
